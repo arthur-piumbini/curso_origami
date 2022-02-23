311787c0ca99c8f880c086f3189719423bc714f1
Arthur Piumbini <arthur.piumbini@intelliway.com.br>
2022-02-23 18:23:25 -0300

    começando...

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("teste ando")
+console.log("testando")
+
+var nome = "arthur"
+let idade = 42 
+const formado = true // não muda seu valor (constante)
+console.log(idade, nome, formado)
+
+var preco = 34,
+    totalComprado = 2,
+    totalPreco = preco * totalComprado
+console.log(totalPreco)
+
+var vasco
+console.log(vasco) //undefined
+
+//hosting ->
+
+console.log(teste)
+var teste = "oioioi" // undefined
+console.log(teste) // "oioioi"
+
